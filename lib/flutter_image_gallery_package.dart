b9022394ef86887c47025f775b5656281f93ff74
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-06-26 16:57:41 +0545

    image gallery added

--- lib/flutter_image_gallery_package.dart
+++ lib/flutter_image_gallery_package.dart
@@ -1,7 +1,3 @@
 library flutter_image_gallery_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_image_gallery_package/flutter_image_gallery.dart';
